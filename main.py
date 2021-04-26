50ddd20dd254af19bca1faa175b452363a06efd1
kperkns2 <kap20k4@gmail.com>
2021-04-26 14:01:16 +0000

    Updated Streamlit App

--- main.py
+++ main.py
@@ -11,28 +11,28 @@ import numpy as np
   
 
 
-def new_img(im_file):
-    global im, fig, ax, coords, ix, iy, ax, cid
-    im = np.array(Image.open(im_file))
-    fig = plt.figure(figsize=[img_size,img_size])
-    ax = fig.add_subplot(111)
-    ax.imshow(im)
-    coords = []
-    def onclick(event):
-        global ix, iy, ax
-        ix, iy = event.xdata, event.ydata
-        global coords
-        coords.append((ix, iy))
-        point_list = ['StickerClose CableFar','StickerFar CableFar','StickerFar CableClose','StickerClose CableClose', 'Coaxial', 'Fiber']
-        ax.scatter(ix,iy, s=20, label=point_list[len(coords)-1])
-        plt.legend(bbox_to_anchor=(1.1, 1.0))
-        if len(coords) == 6:
-            fig.canvas.mpl_disconnect(cid)
-        return coords
-    cid = fig.canvas.mpl_connect('button_press_event', onclick)
-    st.plotly_chart(fig)
+# def new_img(im_file):
+#     global im, fig, ax, coords, ix, iy, ax, cid
+#     im = np.array(Image.open(im_file))
+#     fig = plt.figure(figsize=[img_size,img_size])
+#     ax = fig.add_subplot(111)
+#     ax.imshow(im)
+#     coords = []
+#     def onclick(event):
+#         global ix, iy, ax
+#         ix, iy = event.xdata, event.ydata
+#         global coords
+#         coords.append((ix, iy))
+#         point_list = ['StickerClose CableFar','StickerFar CableFar','StickerFar CableClose','StickerClose CableClose', 'Coaxial', 'Fiber']
+#         ax.scatter(ix,iy, s=20, label=point_list[len(coords)-1])
+#         plt.legend(bbox_to_anchor=(1.1, 1.0))
+#         if len(coords) == 6:
+#             fig.canvas.mpl_disconnect(cid)
+#         return coords
+#     cid = fig.canvas.mpl_connect('button_press_event', onclick)
+#     st.plotly_chart(fig)
 
-f = st.sidebar.file_uploader('File uploader')
+# f = st.sidebar.file_uploader('File uploader')
 
-if f is not None:
-  new_img(f)
+# if f is not None:
+#   new_img(f)

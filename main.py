45da0ee0da01071c8bb7891efc7cbadc679965e1
kperkns2 <kap20k4@gmail.com>
2021-04-26 16:07:21 +0000

    Updated Streamlit App

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ GA_JS = """Here is some JavaScript"""
 
 # Insert the script in the head tag of the static template inside your virtual environement
 index_path = pathlib.Path(st.__file__).parent #/ "static" / "index.html"
-#st.wrtie(index_path)
+st.wrtie(index_path)
 #soup = BeautifulSoup(index_path.read_text(), features="lxml")
 # if not soup.find(id='custom-js'):
 #     script_tag = soup.new_tag("script", id='custom-js')

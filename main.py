cda86befa1dadca6320660d04e611b6a4fd9d013
kperkns2 <kap20k4@gmail.com>
2021-04-27 16:15:43 +0000

    Updated Streamlit App

--- main.py
+++ main.py
@@ -5,31 +5,85 @@ from streamlit_labelstudio import st_labelstudio
 st.set_page_config(layout='wide')
 
 
-config = """<View>
+def polygon_segmentation():
+  config = """<View>
+    <Header value="Select label and start to click on image"/>
+    <Image name="image" value="$image"/>
 
-  <Header value="Select label and start to click on image"/>
-  <Image name="image" value="$image"/>
+    <PolygonLabels name="label" toName="image"
+                  strokeWidth="3" pointSize="small"
+                  opacity="0.9">
+      <Label value="Normal" background="green"/>
+      <Label value="Defective" background="red"/>
+    </PolygonLabels>
+  </View>
+  """    
+  interfaces = [
+    "panel",
+    "update",
+    "controls",
+    "side-column"
+  ],
+  user={}
+  results_raw = st_labelstudio(config, interfaces, user, task)
+  if results_raw is not None:
+    areas = [v for k, v in results_raw['areas'].items()]
+    results = []
+    for a in areas:
+      results.append({'id':a['id'], 'x':[p['x']  for p in  a['points']], 'y':[p['y']  for p in  a['points']],  'label':a['results'][0]['value']['polygonlabels'][0]})
+    st.table(results)
 
-  <PolygonLabels name="label" toName="image"
-                 strokeWidth="3" pointSize="small"
-                 opacity="0.9">
-    <Label value="Normal" background="green"/>
-    <Label value="Defective" background="red"/>
-  </PolygonLabels>
-
-</View>
-"""    
 
 
-interfaces = [
-  "panel",
-  "update",
-  "controls",
-  "side-column"
-],
+def bounding_box():
+  config = """
+        <View>
+          <View style="padding: 25px; box-shadow: 2px 2px 8px #AAA;">
+            <Image name="img" value="$image" width="100%" maxWidth="100%" brightnessControl="true" contrastControl="true" zoomControl="true" rotateControl="true"></Image>
+          </View>
+          <RectangleLabels name="tag" toName="img">
+            <Label value="Hello"></Label>
+            <Label value="Moon"></Label>
+          </RectangleLabels>
+        </View>
+      """
 
+  interfaces = [
+    "panel",
+    "update",
+    "controls",
+    "side-column",
+    "completions:menu",
+    "completions:add-new",
+    "completions:delete",
+    "predictions:menu",
+  ],
 
-user={}
+  user = {
+    'pk': 1,
+    'firstName': "James",
+    'lastName': "Dean"
+  },
+
+  task = {
+    'completions': [],
+    'predictions': [],
+    'id': 1,
+    'data': {
+      'image': "https://htx-misc.s3.amazonaws.com/opensource/label-studio/examples/images/nick-owuor-astro-nic-visuals-wDifg5xc9Z4-unsplash.jpg"
+    }
+  }
+
+  results_raw = st_labelstudio(config, interfaces, user, task)
+
+  if results_raw is not None:
+    areas = [v for k, v in results_raw['areas'].items()]
+
+    results = []
+    for a in areas:
+      results.append({'id':a['id'], 'x':a['x'], 'y':a['y'], 'width':a['width'], 'height':a['height'], 'label':a['results'][0]['value']['rectanglelabels'][0]})
+
+    st.table(results)  
 
 task = {
   'completions': [],
@@ -40,14 +94,9 @@ task = {
   }
 }
 
-results_raw = st_labelstudio(config, interfaces, user, task)
 
 
-if results_raw is not None:
-  areas = [v for k, v in results_raw['areas'].items()]
-
-  results = []
-  for a in areas:
-    results.append({'id':a['id'], 'x':[p['x']  for p in  a['points']], 'y':[p['y']  for p in  a['points']],  'label':a['results'][0]['value']['polygonlabels'][0]})
-
-  st.table(results)
+if st.sidebar.radio(["Bounding Box", "Polygon Segmentation"]) == "Bounding Box":
+  bounding_box()
+else:
+  polygon_segmentation()
